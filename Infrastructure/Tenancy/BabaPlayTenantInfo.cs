f7b38d10cab52849f79562b8db32056903c5c8ec
Ronaldo Estrela <ronaldoestrela@yahoo.com.br>
2026-02-10 10:46:43 -0300

    refactor: downgrade target framework to net8.0 and update package references

--- Infrastructure/Tenancy/BabaPlayTenantInfo.cs
+++ Infrastructure/Tenancy/BabaPlayTenantInfo.cs
@@ -4,13 +4,13 @@ namespace Infrastructure.Tenancy;
 
 public class BabaPlayTenantInfo : ITenantInfo
 {
-    public required string Id { get; set; }
-    public required string Identifier { get; set; }
-    public required string Name { get; set; }
+    public string? Id { get; set; }
+    public string? Identifier { get; set; }
+    public string? Name { get; set; }
     public string? ConnectionString { get; set; }
-    public required string Email { get; set; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
+    public string? Email { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public DateTime ValidUpTo { get; set; }
     public bool IsActive { get; set; }
 }
