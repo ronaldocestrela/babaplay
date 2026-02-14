2cf72b0c9718932b53f59b537bdacf275fc12f90
Ronaldo Estrela <ronaldoestrela@yahoo.com.br>
2026-02-14 09:53:02 -0300

    fix: Initialize Permissions list to an empty array

--- Application/Features/Identity/Roles/RoleResponse.cs
+++ Application/Features/Identity/Roles/RoleResponse.cs
@@ -5,5 +5,5 @@ public class RoleResponse
     public required string Id { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
-    public List<string> Permissions { get; set; } = new List<string>();
+    public List<string> Permissions { get; set; } = [];
 }
